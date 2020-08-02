a43c6d582d8d819b76e8fc376db0905ff9b08a44
YAEGASHI Takeshi <yaegashi@gmail.com>
2020-08-02 19:26:08 +0900

    Parallelize formatting with chan

--- gen/generator_format.go
+++ gen/generator_format.go
@@ -4,27 +4,45 @@ import (
 	"bytes"
 	"io/ioutil"
 	"log"
+	"runtime"
+	"sync"
 
 	"golang.org/x/tools/imports"
 )
 
 func (g *Generator) Format() error {
+	var wgErr error
+	wg := &sync.WaitGroup{}
+	q := make(chan struct{}, runtime.NumCPU())
 	for path := range g.Created {
-		log.Printf("Formatting %s", path)
-		inB, err := ioutil.ReadFile(path)
-		if err != nil {
-			return err
+		if wgErr != nil {
+			break
 		}
-		outB, err := imports.Process(path, inB, nil)
-		if err != nil {
-			return err
-		}
-		if !bytes.Equal(inB, outB) {
-			err = ioutil.WriteFile(path, outB, 0644)
-			if err != nil {
-				return err
+		q <- struct{}{}
+		wg.Add(1)
+		go func(path string) {
+			defer wg.Done()
+			log.Printf("Formatting %s", path)
+			err := func() error {
+				inB, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
+				outB, err := imports.Process(path, inB, nil)
+				if err != nil {
+					return err
+				}
+				if bytes.Equal(inB, outB) {
+					return nil
+				}
+				return ioutil.WriteFile(path, outB, 0644)
+			}()
+			if err != nil && wgErr == nil {
+				wgErr = err
 			}
-		}
+			<-q
+		}(path)
 	}
-	return nil
+	wg.Wait()
+	return wgErr
 }
